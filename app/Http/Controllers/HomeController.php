8317b5d78fb340ee9822512efa0e2fd8c91f8876
Mahmoud Almontasser <almontasser@outlook.com>
2021-05-31 22:55:41 +0200

    Add Stats on Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Project;
+use App\Models\ResearchPaper;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
   public function index()
   {
-    return view('home');
+    $books_count = Book::count();
+    $projects_count = Project::count();
+    $papers_count = ResearchPaper::count();
+    return view('home', [
+      'books_count' => $books_count,
+      'projects_count' => $projects_count,
+      'papers_count' => $papers_count
+    ]);
   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,44 @@
 @section('content')
 
 <div class="content">
+  <div class="row pb-4">
+    <x-overview-block link="{{ route('books-list') }}">
+      <x-slot name="content">
+        <span class="text-dark">{{ $books_count }}</span>
+      </x-slot>
+      <x-slot name="title">
+        <span class="text-dark">عدد الكتب</span>
+      </x-slot>
+    </x-overview-block>
+
+    <x-overview-block link="{{ route('projects-list') }}">
+      <x-slot name="content">
+        <span class="text-dark">{{ $projects_count }}</span>
+      </x-slot>
+      <x-slot name="title">
+        <span class="text-dark">عدد مشاريع التخرج</span>
+      </x-slot>
+    </x-overview-block>
+
+    <x-overview-block link="{{ route('papers-list') }}">
+      <x-slot name="content">
+        <span class="text-dark">{{ $papers_count }}</span>
+      </x-slot>
+      <x-slot name="title">
+        <span class="text-dark">عدد الأوراق البحثية</span>
+      </x-slot>
+    </x-overview-block>
+
+    <x-overview-block link="javascript:void">
+      <x-slot name="content">
+        <span class="text-dark">3,586,125</span>
+      </x-slot>
+      <x-slot name="title">
+        <span class="text-dark">عدد الزوار</span>
+      </x-slot>
+    </x-overview-block>
+  </div>
+
   <h2 class="text-center">عن ماذا تبحث</h2>
   <form action="{{ route('search') }}" method="GET">
     <div class="form-group">
