e0981bcf91593b89e1722389761813cdaf03314b
Mahmoud Almontasser <almontasser@outlook.com>
2023-01-17 18:47:49 +0200

    update to laravel 9

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
     App\Providers\RouteServiceProvider::class,
 
     Mailjet\LaravelMailjet\MailjetServiceProvider::class,
-    Mailjet\LaravelMailjet\MailjetMailServiceProvider::class,
 
   ],
 
