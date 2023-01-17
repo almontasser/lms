b6a2844311175baa0caf6b9da42e5de84d4d65b3
Mahmoud Almontasser <almontasser@outlook.com>
2023-01-17 19:50:29 +0200

    remove unused import

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-use function PHPSTORM_META\type;
-
 class User extends Authenticatable
 {
   use HasFactory, Notifiable;
